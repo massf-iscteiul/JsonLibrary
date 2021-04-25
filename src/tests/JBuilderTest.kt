567a601f2ad2dec4cafadf05655f2da88b4c1749
massf-iscteiul <massf@iscte-iul.pt>
2021-04-25 23:42:00 +0100

    Added JUnit testing for JNull

--- src/tests/JBuilderTest.kt
+++ src/tests/JBuilderTest.kt
@@ -33,20 +33,36 @@ class JBuilderTest {
     }
 
     @Test
+    fun jNullTest() {
+        assertEquals(
+            "null",
+            ModelVisitor().parse(null)
+        )
+    }
+
+    @Test
     fun jArrayTest() {
         assertEquals(
             "[1, 2, \"Harry Potter\", false]",
             ModelVisitor().parse(listOf(1, 2, "Harry Potter", false))
         )
+        assertEquals(
+            "[]",
+            ModelVisitor().parse(mutableListOf<Any>())
+        )
     }
 
     @Test
     fun jObjectTest() {
-        val movie = Movie("Harry Potter", 1)
+        val movie = Movie("Harry Potter", 1, null)
         val movieList = MovieList(10, movie, true, listOf(1, 2, 3))
         assertEquals(
-            "{ \"liked\": true, \"list\": [1, 2, 3], \"movie\": { \"name\": \"Harry Potter\", \"number\": 1 }, \"score\": 10 }",
+            "{\"liked\": true, \"list\": [1, 2, 3], \"movie\": {\"name\": \"Harry Potter\", \"number\": 1, \"series\": null}, \"score\": 10}",
             ModelVisitor().parse(movieList)
+        )
+        assertEquals(
+            "{}",
+            ModelVisitor().parse(Any())
         )
     }
 }
