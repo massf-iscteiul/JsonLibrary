54984d1a33747c67b3f6b93ff6766f12d0c854a8
massf-iscteiul <massf@iscte-iul.pt>
2021-05-22 21:20:43 +0100

    Fixed bugs.

--- src/plugins/WriteJson.kt
+++ src/plugins/WriteJson.kt
@@ -15,29 +15,51 @@ class WriteJson : ActionsPlugin {
         get() = "Write to Json"
 
     override fun execute(jTree: JTree) {
-        val newShell = Shell(jTree.shell)
-        newShell.text = "Write Json"
-        newShell.layout = GridLayout(1, false)
-        val composite = Composite(newShell, 3)
+        val writeJsonShell = Shell(jTree.shell)
+        writeJsonShell.text = name
+        writeJsonShell.layout = GridLayout(1, false)
+        val composite = Composite(writeJsonShell, 2)
         composite.layout = GridLayout(2, false)
         val pathLabel = Label(composite, SWT.WRAP or SWT.SINGLE)
         pathLabel.text = "Choose path:"
         val path = Text(composite, SWT.BORDER)
-        val sendButton = Button(newShell, SWT.PUSH)
+        val sendButton = Button(writeJsonShell, SWT.PUSH)
         sendButton.layoutData = GridData(GridData.FILL_HORIZONTAL)
         sendButton.text = "Write"
-        newShell.open()
-        newShell.pack()
+        writeJsonShell.open()
+        writeJsonShell.pack()
         sendButton.addSelectionListener(object : SelectionAdapter() {
             override fun widgetSelected(e: SelectionEvent) {
-                val fileName = path.text
-                val jsonFile = File(fileName)
-                jsonFile.printWriter().use { out ->
-                    out.println(jTree.jsonLabel.text)
+                if (path.text == ""){
+                    showTryAgain(writeJsonShell)
+                }else {
+                    val fileName = path.text
+                    val jsonFile = File(fileName)
+                    jsonFile.printWriter().use { out ->
+                        out.println(jTree.jsonLabel.text)
+                    }
+                    writeJsonShell.close()
                 }
-                newShell.close()
             }
         })
+    }
+
+    private fun showTryAgain(writeJsonShell: Shell){
+        val tryAgainShell = Shell(writeJsonShell)
+        tryAgainShell.text = "Error"
+        tryAgainShell.layout = GridLayout(1, false)
+        val tryAgainLabel = Label(tryAgainShell, SWT.NONE)
+        tryAgainLabel.text = "No path given. Try Again!"
+        val sendButton = Button(tryAgainShell, SWT.PUSH)
+        sendButton.layoutData = GridData(GridData.FILL_HORIZONTAL)
+        sendButton.text = "OK"
+        sendButton.addSelectionListener(object : SelectionAdapter() {
+            override fun widgetSelected(e: SelectionEvent) {
+                tryAgainShell.close()
+            }
+        })
+        tryAgainShell.open()
+        tryAgainShell.pack()
     }
 
 }
