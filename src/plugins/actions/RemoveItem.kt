06b815ea50f437070eefc36f2d045f8c28d30f85
massf-iscteiul <massf@iscte-iul.pt>
2021-05-24 16:38:46 +0100

    Fixed bug when removing root object.

--- src/plugins/actions/RemoveItem.kt
+++ src/plugins/actions/RemoveItem.kt
@@ -2,6 +2,14 @@ package plugins.actions
 
 import JTree
 import objects.JObject
+import org.eclipse.swt.SWT
+import org.eclipse.swt.events.SelectionAdapter
+import org.eclipse.swt.events.SelectionEvent
+import org.eclipse.swt.layout.GridData
+import org.eclipse.swt.layout.GridLayout
+import org.eclipse.swt.widgets.Button
+import org.eclipse.swt.widgets.Label
+import org.eclipse.swt.widgets.Shell
 
 class RemoveItem : ActionsPlugin {
 
@@ -10,10 +18,33 @@ class RemoveItem : ActionsPlugin {
 
     override fun execute(jTree: JTree) {
         val treeItem = jTree.tree.selection.first()
-        (treeItem.parentItem.data as JObject).objects.removeIf { keyValuePair ->
-            keyValuePair.value == treeItem.data
+        if (treeItem.parentItem != null) {
+            (treeItem.parentItem.data as JObject).objects.removeIf { keyValuePair ->
+                keyValuePair.value == treeItem.data
+            }
+            jTree.refresh()
         }
-        jTree.refresh()
+        else {
+            showShell(jTree.shell)
+        }
     }
 
+
+    private fun showShell(editShell: Shell){
+        val tryAgainShell = Shell(editShell)
+        tryAgainShell.text = "Error"
+        tryAgainShell.layout = GridLayout(1, false)
+        val tryAgainLabel = Label(tryAgainShell, SWT.NONE)
+        tryAgainLabel.text = "Cannot Remove Root Object!"
+        val okButton = Button(tryAgainShell, SWT.PUSH)
+        okButton.layoutData = GridData(GridData.FILL_HORIZONTAL)
+        okButton.text = "OK"
+        okButton.addSelectionListener(object : SelectionAdapter() {
+            override fun widgetSelected(e: SelectionEvent) {
+                tryAgainShell.close()
+            }
+        })
+        tryAgainShell.open()
+        tryAgainShell.pack()
+    }
 }
