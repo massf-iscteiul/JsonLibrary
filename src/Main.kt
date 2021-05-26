ebe146f81c2b0d99de1d3af19e5acf42f32b7b44
massf-iscteiul <massf@iscte-iul.pt>
2021-05-26 14:58:47 +0100

    Cleaned main.

--- src/Main.kt
+++ src/Main.kt
@@ -3,16 +3,10 @@ import tests.testobjects.MovieList
 import apps.JBuilder
 import apps.JTreeWindow
 
-data class Something(val movieList: List<Movie>, val something: String)
-
 fun main() {
     val movie = Movie("Harry Potter", 1, null)
     val movieList = MovieList(10, movie, true, listOf(1, 2, 3))
-    val stringTest = "Hello World"
-    val something = Something(listOf(movie, movie,movie), "SomethingTest")
     val builder = JBuilder().instantiate(movieList)
-    //println(builder.toString())
     val w = Injector.create(JTreeWindow(builder))
     w.open()
-
 }
